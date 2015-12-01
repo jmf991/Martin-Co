a4832380ee6286982ba3501ddde20d58c6e71203
Javier <jmf991@hotmail.com>
2015-12-01 16:55:37 +0100

    no message

--- Martin-co/Martin-co/assets/scripts/functions.js
+++ Martin-co/Martin-co/assets/scripts/functions.js
@@ -340,12 +340,14 @@ function updateIsotope() {
 
 
 function startMap() {
+    if ($("#js-map").length) {
+        var map = new google.maps.Map(document.getElementById('js-map'), {
+            center: new google.maps.LatLng(44.5403, -78.5463),
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scrollwheel: false
+        });
+    }
 
-
-    var map = new google.maps.Map(document.getElementById('js-map'), {
-        center: new google.maps.LatLng(44.5403, -78.5463),
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        scrollwheel: false
-    });
+    
 }
